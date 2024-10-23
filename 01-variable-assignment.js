340c48fcb94c3489b2cdb4bc7c1e54874ea4ca39
chinmay8445 <129591317+chinmay8445@users.noreply.github.com>
2024-10-23 16:41:03 +0530

    variable assingment 02

--- 01-variable-assignment.js
+++ 01-variable-assignment.js
@@ -1,17 +1,19 @@
 
+//variable declaration
 var city;
 var bestfriend;
 var country;
 var school;
 var college;
 
+// variable initilization
 city="Sangli";
 bestfriend="Akash";
 country="India";
 school="P.J.N High school";
 college="A.K. Jc college";
 
-
+//  print values
 console.log("City :",city);
 console.log("Best Friend :",bestfriend);
 console.log("Country :",country);
---
+++ 02-variable-assignment.js
@@ -0,0 +1,40 @@
+
+
+// Declaration 
+var myFullName;
+console.log("My Full Name :",myFullName);
+
+//Initialization
+myFullName="Chinmay Jawale";
+console.log("Name :",myFullName);
+
+//Updation
+myFullName="Chinmay Gajanan Jawale";
+console.log("My Full Name :",myFullName);
+
+var myLove;
+myLove="I love only JS";
+console.log(myLove);
+
+
+var firstName="Chinmay";
+var lastName="Jawale";
+var age="23";
+var address="Pune";
+var pinCode="415306";
+var gender="Male";
+var isMarried="False";
+
+console.log("First Name :",firstName);
+console.log("Last Name :",lastName);
+console.log("Age :",age);
+console.log("Address :",address);
+console.log("Pincode :",pinCode);
+console.log("Gender :",gender);
+console.log("IsMarried :",isMarried);
+
+
+
+
+
+
