9aa6baa2afb69a2dabc641ec0e10c8df17e63c9e
chinmay8445 <129591317+chinmay8445@users.noreply.github.com>
2024-10-23 16:05:21 +0530

    variable assignment 1

---
+++ 01-variable-assignment.js
@@ -0,0 +1,23 @@
+
+var city;
+var bestfriend;
+var country;
+var school;
+var college;
+
+city="Sangli";
+bestfriend="Akash";
+country="India";
+school="P.J.N High school";
+college="A.K. Jc college";
+
+
+console.log("City :",city);
+console.log("Best Friend :",bestfriend);
+console.log("Country :",country);
+console.log("School :", school);
+console.log("Coolege :",college);
+
+
+
+
